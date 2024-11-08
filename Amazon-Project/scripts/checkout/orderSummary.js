2da54c8b300f7563dafe34223603ed1639e9393a
Babur Shasaidov <shasaidovbabur@gmail.com>
2024-11-08 19:32:46 +0800

    Create Automated tests for order summary

--- Amazon-Project/scripts/checkout/orderSummary.js
+++ Amazon-Project/scripts/checkout/orderSummary.js
@@ -50,7 +50,7 @@ export function renderOrderSummary() {
 
     // Add HTML for each cart item, including details like image, name, price, and quantity controls
     cartSummaryHTML += `
-      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+      <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
           Delivery date: ${dateString}
         </div>
@@ -65,7 +65,7 @@ export function renderOrderSummary() {
             <div class="product-price">
               $${formatCurrency(matchingProduct.priceCents)}
             </div>
-            <div class="product-quantity">
+            <div class="product-quantity js-product-quantity-${matchingProduct.id}">
               <span>
                 Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
               </span>
@@ -74,7 +74,8 @@ export function renderOrderSummary() {
               </span>
               <input class="quantity-input js-quantity-input-${matchingProduct.id}">
               <span class="save-quantity-link link-primary js-save-link" data-product-id="${matchingProduct.id}">Save</span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+              <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" 
+              data-product-id="${matchingProduct.id}">
                 Delete
               </span>
             </div>
@@ -136,7 +137,8 @@ export function renderOrderSummary() {
   }
 
   // Insert the cart HTML into the page
-  document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+  document.querySelector('.js-order-summary')
+    .innerHTML = cartSummaryHTML;
 
   // Add event listeners to each "Delete" link
   document.querySelectorAll('.js-delete-link').forEach((link) => {

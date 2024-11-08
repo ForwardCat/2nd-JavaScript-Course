eb3d223edefeeaee5d39199cab742bde261b2644
Babur Shasaidov <shasaidovbabur@gmail.com>
2024-11-08 14:32:08 +0800

    Create automated tests for addToCart

--- Amazon-Project/data/cart.js
+++ Amazon-Project/data/cart.js
@@ -1,16 +1,22 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) 
+export let cart;
 
-if (!cart) {
-  cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-  }, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-  }];   
-}
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }, {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }];   
+  };
+};
 
 export function calculateCartQuantity() {
   let cartQuantity = 0;
@@ -27,9 +33,7 @@ function saveToStorage() {
 }
 
 export function addToCart(productId) {
-  const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`)
-  let quantity = Number(quantitySelector.value);
-
+  
   let matchingItem;
 
   cart.forEach((cartItem) => {
@@ -39,12 +43,12 @@ export function addToCart(productId) {
   });
 
   if(matchingItem) {
-    matchingItem.quantity += quantity;
+    matchingItem.quantity += 1;
   } else {
     cart.push({
       productId: productId,
-      quantity: quantity,
-      deliveryOptionsId: '1'
+      quantity: 1,
+      deliveryOptionId: '1'
     });     
   }
 
@@ -91,3 +95,4 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
 
   saveToStorage();
 }
+

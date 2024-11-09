c390b13f63d6742e1212bca883fa36d1d79efb58
Babur Shasaidov <shasaidovbabur@gmail.com>
2024-11-09 13:41:30 +0800

    Revert "Create automated tests for formatCurrency"
    This reverts commit 3e0a49b6dc4b38c9033925191778f176a61281ff.

--- Amazon-Project/tests/checkout/orderSummaryTest.js
+++ Amazon-Project/tests/checkout/orderSummaryTest.js
@@ -6,6 +6,9 @@ describe('test suite: renderOrderSummary', () => {
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d'
 
+  const product1Name = 'Black and Gray Athletic Cotton Socks - 6 Pairs';
+  const product2Name = 'Intermediate Size Basketball';
+
 // Hook (Before Each)
   beforeEach(() => {
     spyOn(localStorage, 'setItem');
@@ -32,6 +35,11 @@ describe('test suite: renderOrderSummary', () => {
     renderOrderSummary();
   });
 
+  // Hook (afterEach)
+  afterEach(() => {
+    document.querySelector('.js-test-container').innerHTML = '';
+  });
+
 
 // Test 1
   it('displays the cart', () => {
@@ -42,11 +50,18 @@ describe('test suite: renderOrderSummary', () => {
     expect(
       document.querySelector(`.js-product-quantity-${productId1}`).innerText
     ).toContain('Quantity: 2');
+
     expect(
       document.querySelector(`.js-product-quantity-${productId2}`).innerText
     ).toContain('Quantity: 1');
 
-    document.querySelector('.js-test-container').innerHTML = '';
+    expect(
+      document.querySelector(`.js-product-name-${productId1}`).innerText
+    ).toContain(product1Name);
+
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText
+    ).toContain(product2Name);
   });
 
 
@@ -54,18 +69,25 @@ describe('test suite: renderOrderSummary', () => {
 // Test 2
   it('removes a product', () => {
     document.querySelector(`.js-delete-link-${productId1}`).click();
+
     expect(
       document.querySelectorAll(`.js-cart-item-container`).length
     ).toEqual(1);
+
     expect(
       document.querySelector(`.js-cart-item-container-${productId1}`)
     ).toEqual(null);
+
     expect(
       document.querySelector(`.js-cart-item-container-${productId2}`)
     ).not.toEqual(null);
+
     expect(cart.length).toEqual(1);
+
     expect(cart[0].productId).toEqual(productId2);
 
-    document.querySelector('.js-test-container').innerHTML = '';
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText
+    ).toContain(product2Name);
   })
 });
--- Amazon-Project/tests/data/cartTest.js
+++ Amazon-Project/tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import {addToCart, cart, loadFromStorage} from '../../data/cart.js'; 
+import {addToCart, cart, loadFromStorage, removeFromCart} from '../../data/cart.js'; 
 
 
 // Define a test suite for the addToCart function
@@ -61,5 +61,28 @@ describe('test suite: addToCart', () => {
 
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6'); // Check that the new product's ID matches the expected ID
     expect(cart[0].quantity).toEqual(1); // Verify that the quantity is set to 1 for the new product
+  });
+});
+
+
+
+describe('Test Suite: Remove from cart function', () => {
+  beforeEach(() => {
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      // Mock localStorage.getItem to return a predefined cart with one item
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
+    });
+
+    spyOn(localStorage.setItem); 
+  });
+  
+  it('remove a productId that is in the cart', () => {
+    removeFromCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+
+    expect(cart.length).toEqual(0);
   });
 });

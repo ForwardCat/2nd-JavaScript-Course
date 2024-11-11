8441e4aca266a75b17c46c67323c988d5b502d4f
Babur Shasaidov <shasaidovbabur@gmail.com>
2024-11-11 17:20:01 +0800

    Challenge exercises for Testing frameworks

--- Amazon-Project/tests/checkout/orderSummaryTest.js
+++ Amazon-Project/tests/checkout/orderSummaryTest.js
@@ -112,4 +112,26 @@ describe('Test Suite: renderOrderSummary', () => {
       document.querySelector(`.js-product-price-${productId2}`).innerText
     ).toEqual('$20.95');
   });
+
+
+  // Test 3
+  it('updating the delivery option', () => {
+    // choose the third delivery option on the first product
+    document.querySelector(`.js-delivery-option-${productId1}-3`).click();
+
+    // check if the third delivery option of the first product is checked
+    expect(
+      document.querySelector(`.js-delivery-option-input-${productId1}-3`).checked      
+    ).toEqual(true);
+    expect(cart.length).toEqual(2);
+    expect(cart[0].productId).toEqual(productId1);
+    expect(cart[0].deliveryOptionId).toEqual('3');
+
+    expect(
+      document.querySelector(`.js-payment-summary-shipping`).innerText
+    ).toEqual('$14.98');
+    expect(
+      document.querySelector(`.js-payment-summary-total`).innerText
+    ).toEqual('$63.50');
+  });
 });
--- Amazon-Project/tests/data/cartTest.js
+++ Amazon-Project/tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
+import { addToCart, cart, loadFromStorage, removeFromCart } from "../../data/cart.js";
 
 describe('Test Suite: addToCart', () => {
   beforeEach(() => {
@@ -45,6 +45,55 @@ describe('Test Suite: addToCart', () => {
     expect(cart[0].quantity).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
       productId: "0001",
+      quantity: 1,
+      deliveryOptionId: '1'
+    }]));
+  });
+});
+
+// New test suite
+describe('Test Suite: removeFromCart', () => {
+  beforeEach(() => {
+    spyOn(localStorage, 'setItem');
+  });
+
+  // Test 1
+  it('removes a productId that is in the cart', () => {
+    // create a mock of localStorage.getItem and create a mock cart
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
+    });
+    // have to localStorage
+    loadFromStorage();
+
+    // try removing an existing product from the cart
+    removeFromCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    // checking if the cart is empty
+    expect(cart.length).toEqual(0);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([]));
+  });
+
+  // Test 2
+  it('removes a productId that is not in the cart', () => {
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1,
+        deliveryOptionId: '1'
+      }]);
+    });
+    loadFromStorage();
+
+    removeFromCart('fakeId');
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
       quantity: 1,
       deliveryOptionId: '1'
     }]));
